115a47675d59f995657e86398d49f0afa13d7547
Filip Drażba <fdrazba@gmail.com>
2023-04-15 19:36:20 +0200

    Initial commit.

---
+++ src/main/java/pl/edu/pb/et/EtApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pb.et;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/edu/pb/et/EtApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pb.et;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
